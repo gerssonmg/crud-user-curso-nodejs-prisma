2a5f4800f1bf1851f1d9d98dcf7db3eddb1da92f
gerson aguiar <gersoncafilho@gmail.com>
2024-09-12 21:32:56 -0300

    crud primeiras querys

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
+<!--
 pnpm i typescript @types/node tsx tsup -D
 
 npx tsc --init
 
-
 pnpm i prisma -D
 
 pnpm i dotenv
-
+-->
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,62 @@ const prisma = new PrismaClient({
     log:  ['query'],
   });
 
-console.log("Server aula prisma")
+console.log("Server aula prisma 01")
+
+prisma.user.create({
+    data: {
+        name: 'Gerson2',
+        email: 'gerson4@gmail.com'
+    }
+}).then((result) => {
+  console.log(result)}).catch((error) => {
+    console.log("DEU ERROR")
+  })
+
+  /**
+  prisma:query
+  INSERT INTO `main`.`User` (`email`, `name`) 
+  VALUES (?,?) 
+  RETURNING `id` AS `id`, `email` AS `email`, `name` AS `name`
+  { id: 2, email: 'gerson1@gmail.com', name: 'Gerson1' }
+  */
+
+prisma.user.delete({
+  where: {
+    email: 'gerson2@gmail.com'}
+}).then(() => {}).catch((error) => {
+console.log(error)
+console.log("DEU ERROR no DELETE 2")
+})
+
+prisma.user.update({
+    where: {
+        email: 'gerson3@gmail.com'
+    },
+    data: {
+        name: 'Gerson Atualizado'
+    }}).then((result) => {
+      console.log("DEU CERTO O UPDATE")
+    })
+
+/**
+prisma:query UPDATE `main`.`User` SET `name` = ? WHERE (`main`.`User`.`email` = ? AND 1=1) RETURNING `id` AS `id`, `email` AS `email`, `name` AS `name`
+*/
+
+prisma.user.findMany().then((result) => {
+    result.forEach((user) => {
+      if(user.email === 'gerson3@gmail.com'){
+        console.log(user)
+      }
+    })
+  })
+
+/**
+prisma:query SELECT `main`.`User`.`id`,
+  `main`.`User`.`email`, `main`.`User`.`name` 
+  FROM `main`.`User` WHERE 1=1 LIMIT ? OFFSET ?
+*/
+
+  prisma.user.findFirst().then((result) => {
+    console.log(result)
+  })
