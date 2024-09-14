f465314f3f6ec646bbc03fcc6d4cbf1dfe6d29b7
gerson aguiar <gersoncafilho@gmail.com>
2024-09-14 12:41:38 -0300

    criando rotas - dados MOCK - tipando request

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,82 @@
-console.log('Criando API')
-// Require the framework and instantiate it
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
 
-// ESM
-import Fastify from 'fastify'
-
-const fastify = Fastify({
-  logger: true,
+const app = Fastify({
+  logger: false,
 })
+
+const users = [
+  { id: 1, name: 'Gerson', email: 'gerson@gmail.com' },
+  { id: 2, name: 'Ana', email: 'ana@gmail.com' },
+]
 
 // Rota GET
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+app.get('/list-users', function (request, reply) {
+  reply.send(users)
 })
 
+interface IUserBody {
+  name: string
+  email: string
+}
+
+interface IUserParams {
+  id: number
+}
+
+app.post(
+  '/create-user2',
+  function (
+    request: FastifyRequest<{
+      Body: IUserBody
+    }>,
+    reply: FastifyReply,
+  ) {
+    console.log(request.body)
+    const { name, email } = request.body
+
+    users.push({
+      id: users.length + 1,
+      name,
+      email,
+    })
+    reply.send('usuario criado com sucesso')
+  },
+)
+
+app.put(
+  '/update-user/:id',
+  function (
+    request: FastifyRequest<{ Body: IUserBody; Params: IUserParams }>,
+    reply,
+  ) {
+    const { id } = request.params
+    console.log(request.params)
+
+    const { name, email } = request.body
+
+    users[id - 1] = {
+      id: id,
+      name: name,
+      email: email,
+    }
+
+    reply.send('usuario atualizado com sucesso')
+  },
+)
+
+app.delete(
+  '/delete-user/:id',
+  function (request: FastifyRequest<{ Params: IUserParams }>, reply) {
+    users.splice(request.params.id - 1, 1)
+
+    reply.send()
+  },
+)
+
 // Run the server!
-fastify.listen({ port: 3000 }, function (err) {
+app.listen({ port: 3000 }, function (err) {
   if (err) {
-    fastify.log.error(err)
+    app.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
 })
